6dd82676786dd0f8eda0f219c3d48fc73413274a
MOkhten <m.okhten@gmail.com>
2023-04-03 14:06:24 +0300

    email verification

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const { User } = require('../models/user');
 const path = require('path');
 const fs = require('fs/promises');
 const Jimp = require("jimp");
+const { NotFound } = require('http-errors');
 
 const getCurrent = async (req, res) => {
    
@@ -42,8 +43,21 @@ const updateAvatar = async (req, res) => {
     }
 };
 
+const verifyEmail = async (req, res) => {
+    const { verificationToken } = req.params;
+    const user = await User.findOne({ verificationToken });
+    if (!user) {
+        throw NotFound();
+    }
+    await User.findByIdAndUpdate(user._id, { verify: true, verificationToken: null });
+    res.json({
+        "message": "Verification email sent"
+    })
+}
+
 
 module.exports = {
     getCurrent,
     updateAvatar,
+    verifyEmail,
 }
